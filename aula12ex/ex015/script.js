afeeb0e7f652bb7954c2ed2af09fb6e3b0735638
Joseildo Andrade <140559913+joseildoandrade12@users.noreply.github.com>
2023-11-16 18:23:41 -0300

    termino de exercicio verificador de idade

---
+++ aula12ex/ex015/script.js
@@ -0,0 +1,40 @@
+function calcular(){
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.getElementById('res')
+    if (fano.value.length == 0 || fano.value > ano) {
+        window.alert('[ERROR] Verifique os dados e tente novamente!')
+    } else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if(fsex[0].checked){
+            genero = 'Homem'
+            if (idade >= 0 && idade < 10) {
+                img.setAttribute('src', 'bb-homem.jpg')
+            } else if ( idade < 21){
+                img.setAttribute('src', 'jovem-homem.jpg' )
+            } else if ( idade < 50){
+                img.setAttribute('src', 'adulto-homem.jpg')
+            } else {
+                img.setAttribute('src', 'idoso-homem.jpg')
+            }
+        } else if (fsex[1].checked) {
+            genero = 'Mulher'
+            if(idade >= 0 && idade < 10){
+                img.setAttribute('src', 'bb-mulher.jpg')
+            } else if (idade < 21) {
+                img.setAttribute('src', 'jovem-mulher.jpg')
+            } else if (idade < 50) {
+                img.setAttribute('src', 'adulta-mulher.jpg')
+            } else {
+                img.setAttribute('src', 'idosa-mulher.jpg')
+            }
+        }
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos`
+        res.appendChild(img)
+    }
+}
