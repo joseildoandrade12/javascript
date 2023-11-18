18459fb52947be79b6502ba985d581850c9c2362
Joseildo Andrade <140559913+joseildoandrade12@users.noreply.github.com>
2023-11-18 14:15:51 -0300

    estudos Javascript Repetições

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -1,16 +1,21 @@
 function calcular(){
+
     var data = new Date()
     var ano = data.getFullYear()
+
     var fano = document.getElementById('txtano')
     var res = document.getElementById('res')
+
     if (fano.value.length == 0 || fano.value > ano) {
         window.alert('[ERROR] Verifique os dados e tente novamente!')
     } else {
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
         var genero = ''
+
         var img = document.createElement('img')
-        img.setAttribute('id', 'foto')
+        img.setAttribute('id', 'foto') //criação de uma img com id de valor foto
+
         if(fsex[0].checked){
             genero = 'Homem'
             if (idade >= 0 && idade < 10) {
---
+++ aula13/node_b92ce9ae0599b.tmp.js
@@ -0,0 +1,18 @@
+var c = 1
+while (c <= 600 ){
+    console.log('Tudo bem')
+
+}
+
+
+
+
+
+/*
+console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem')
+*/
